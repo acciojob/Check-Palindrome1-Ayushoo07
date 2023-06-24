806d09429c101191fa55316a93c0f3c049b88625
Ayush Singh <akhileshayush.2016@gmail.com>
2023-06-24 12:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+str=str.toLowerCase();
+	let temp = str;
+	for (let index = 0; index < str.length; index++) {
+		if(str.charAt(index)!=str.charAt(str.length-1-index)){
+			return false;
+		}
+			
+	}
+	return true;
 }
 module.exports = palindrome
